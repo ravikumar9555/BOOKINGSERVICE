b55b4b4aed09da1a407e247d0b55d71e22e50312
“ravikumar9555” <ravikumar7518989121@email.com>
2025-12-21 11:16:57 +0530

    Added create booking repository

---
+++ src/utils/errors/index.js
@@ -0,0 +1,7 @@
+
+
+module.exports={
+    ValidationError:require('../errors/validation-error'),
+    AppError:require('../errors/app-errors'),
+    ServiceError:require('../errors/service-error')
+}
