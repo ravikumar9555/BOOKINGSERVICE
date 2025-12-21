946a1d03181a5a112260c81758e02ac2fc1369f1
“ravikumar9555” <ravikumar7518989121@email.com>
2025-12-21 10:53:01 +0530

    Error Setup

---
+++ src/utils/errors/service-error.js
@@ -0,0 +1,19 @@
+const { StatusCodes, INTERNAL_SERVER_ERROR} = require('http-status-codes')
+
+class ServiceError extends Error{
+    constructor(
+        message = 'Something went wrong',
+         explanation='Service layer error'
+         , statusCodes= StatusCodes.INTERNAL_SERVER_ERROR){
+
+            super();
+                    this.name = 'ServiceError',
+        this.message=this.message,
+        this.explanation=this.explanation,
+        this.statusCodes=this.statusCodes
+
+    }
+    
+}
+
+module.exports = ServiceError;
